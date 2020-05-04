11fb7854f62653a3ac00fd4e6dba80b831b435db
Lenya Sychev <lenya2012@mail.ru>
2020-05-04 04:17:54 +0300

    Jптимизирован код

--- model/application.py
+++ model/application.py
@@ -2,8 +2,6 @@
 from wsgiref import util
 
 from model.response_body_class import ResponseBodyClass
-from model.response_headers_class import ResponseHeadersClass
-from model.status_codes_class import StatusCodeClass
 from model.request_date_class import RequestClintClass
 
 
@@ -20,7 +18,6 @@ class AppClass(object):
 
     # вызов обьекта класса во время запроса.
     def __iter__(self):
-        # ! Класc обработки запроса входящего
         self.request_date.url_path_info = util.shift_path_info(self.environ)
         self.start(self.request_date.route().status_code, self.request_date.get_headers())
         response_body = self.response_body.create_string_body(file_patch=self.request_date.route().file_request)
